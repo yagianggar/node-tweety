0661d53d4c6b0d4041b4e6fd5ecc3973bdae5d91
Yagi Anggar Prahara <yagi@wego.com>
2014-05-11 23:37:32 +0700

    Added error checking & some logs of database connection

--- libs/database.js
+++ libs/database.js
@@ -9,10 +9,12 @@ exports.connect = function (req, res) {
 	});
 
 	connection.connect(function(err) {
-	  console.log("On connect : ");
-	  console.log(err);
+	  console.log("Connecting Database ...");
 	  if (err) {
 	  	console.log("DB Connection Error : "+err.code);
+	  	console.log(err);
+	  } else {
+	  	console.log("DB Successfully Connected!!!");
 	  }
 	});
 	return connection;
